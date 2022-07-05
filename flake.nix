7c41e46aa620bfe2d586a0c9c75fec4315a0e6a9
Sam Balco <goodlyrottenapple@gmail.com>
2022-07-05 13:11:22 +0100

    cleanup + nixfmt

--- flake.nix
+++ flake.nix
@@ -1,48 +1,47 @@
 {
   description = "pynixify - Nix expression generator for Python projects";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-    overlay = final: prev: let
-      packageOverrides = import ./nix/overlay.nix;
-    in {
-      python2 = prev.python2.override { inherit packageOverrides; };
-      python27 = prev.python27.override { inherit packageOverrides; };
-      python3 = prev.python3.override { inherit packageOverrides; };
-      python35 = prev.python35.override { inherit packageOverrides; };
-      python36 = prev.python36.override { inherit packageOverrides; };
-      python37 = prev.python37.override { inherit packageOverrides; };
-      python38 = prev.python38.override { inherit packageOverrides; };
-      python39 = prev.python39.override { inherit packageOverrides; };
-      python310 = prev.python310.override { inherit packageOverrides; };
+      overlay = final: prev:
+        prev.lib.genAttrs [
+          "python2"
+          "python27"
+          "python3"
+          "python35"
+          "python36"
+          "python37"
+          "python38"
+          "python39"
+          "python310"
+        ] (python:
+          prev.${python}.override {
+            packageOverrides = import ./nix/overlay.nix;
+          }) // {
+            pynixify = final.python3.pkgs.toPythonApplication
+              (final.python3.pkgs.pynixify.overridePythonAttrs (drv: {
+                # Add system dependencies
+                checkInputs = drv.checkInputs
+                  ++ [ prev.nix prev.nixfmt prev.bats ];
 
+                postInstall = ''
+                  # Add nixfmt to pynixify's PATH
+                  wrapProgram $out/bin/pynixify --prefix PATH : "${prev.nixfmt}/bin"
+                '';
+              }));
+          };
 
-      pynixify = final.python3.pkgs.toPythonApplication (final.python3.pkgs.pynixify.overridePythonAttrs
-      (drv: {
-        # Add system dependencies
-        checkInputs = drv.checkInputs ++ [ prev.nix prev.nixfmt prev.bats ];
-
-        postInstall = ''
-          # Add nixfmt to pynixify's PATH
-          wrapProgram $out/bin/pynixify --prefix PATH : "${prev.nixfmt}/bin"
-        '';
-      }));
-    }; in
-
-
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [ overlay ];
-        }; in
-      rec {
+        };
+      in rec {
         packages.pynixify = pkgs.pynixify;
         defaultPackage = packages.pynixify;
-      }
-    ) // {
-      inherit overlay;
-    };
+      }) // {
+        inherit overlay;
+      };
 }
